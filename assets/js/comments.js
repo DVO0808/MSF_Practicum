b6dd90e587bec24460a47d93d4fa727927e436cf
DVO0808 <damilola.olatayou@utexas.edu>
2016-10-12 10:00:48 -0500

    comment box in all sections

--- assets/js/comments.js
+++ assets/js/comments.js
@@ -28,6 +28,8 @@ var myDiv = document.getElementById("myDiv");
 */
 
 
+
+
 function submit(){
   
 var name = $("#name").val().trim();
@@ -49,5 +51,77 @@ $("#myDiv").append(h3);
    $("#userText").val("")
     
 }
+
+
+function submit2(){
+  
+var name = $("#name2").val().trim();
+var userText = $("#userText2").val().trim();
+//var myDiv = document.getElementById("myDiv");
+d = moment().format('MMM Do YYYY, h:mm a');
+    console.log(d);
+
+var p = $("<p>").html(d);
+var h2 = $("<h2>").html(name);
+var h3 = $("<h3>").html(userText);
+
+
+$("#myDiv2").append(p);
+$("#myDiv2").append(h2);
+$("#myDiv2").append(h3);
+    
+$("#name2").val("");
+$("#userText2").val("")
+    
+}
+
+
+
+function submit3(){
+  
+var name = $("#name3").val().trim();
+var userText = $("#userText3").val().trim();
+//var myDiv = document.getElementById("myDiv");
+d = moment().format('MMM Do YYYY, h:mm a');
+    console.log(d);
+
+var p = $("<p>").html(d);
+var h2 = $("<h2>").html(name);
+var h3 = $("<h3>").html(userText);
+
+
+$("#myDiv3").append(p);
+$("#myDiv3").append(h2);
+$("#myDiv3").append(h3);
+    
+$("#name3").val("");
+$("#userText3").val("")
+    
+}
+
+
+function submit4(){
+  
+var name = $("#name4").val().trim();
+var userText = $("#userText4").val().trim();
+//var myDiv = document.getElementById("myDiv");
+d = moment().format('MMM Do YYYY, h:mm a');
+    console.log(d);
+
+var p = $("<p>").html(d);
+var h2 = $("<h2>").html(name);
+var h3 = $("<h3>").html(userText);
+
+
+$("#myDiv4").append(p);
+$("#myDiv4").append(h2);
+$("#myDiv4").append(h3);
+    
+$("#name4").val("");
+$("#userText4").val("")
+    
+}
+
+
 
 
